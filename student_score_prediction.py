9644e2e5bf596e0b533c3a032e718a7dde15c346
lucs-collab <lucs43177@gmail.com>
2025-05-11 13:26:25 +0530

    Update student_score_prediction.py

--- student_score_prediction.py
+++ student_score_prediction.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,8 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-
-url = "http://bit.ly/w-data"
+url = r"C:\Users\wwwlu\Documents\student_scores.csv"
 df = pd.read_csv(url)
 
 
@@ -18,25 +18,28 @@ plt.grid(True)
 plt.show()
 
 
-X = df[['Hours']] 
-y = df['Scores']  
+X = df[['Hours']]
+y = df['Scores']
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+
 model = LinearRegression()
 model.fit(X_train, y_train)
+
 
 y_pred = model.predict(X_test)
 
 
-print("Mean Squared Error:", mean_squared_error(y_test, y_pred)) 
-print("R2 Score:", r2_score(y_test, y_pred)) 
+print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
 
-line = model.coef_ * X + model.intercept_ 
 
-plt.scatter(X, y, color='blue') 
-plt.plot(X, line, color='red')   
+line = model.coef_ * X + model.intercept_
+
+plt.scatter(X, y, color='blue')
+plt.plot(X, line, color='red')
 plt.title('Regression Line: Study Hours vs Scores')
 plt.xlabel('Hours Studied')
 plt.ylabel('Score')
@@ -45,13 +48,13 @@ plt.show()
 
 
 hours = int(input("whats the hour"))
-hours = np.array([[hours]]) 
+hours = np.array([[hours]])
 predicted_score = model.predict(hours)
 print(f"Predicted score for {hours[0][0]} study hours = {predicted_score[0]:.2f}")
 
 
 plt.scatter(y_test, y_pred)
-plt.plot([0, 100], [0, 100], color='red') 
+plt.plot([0, 100], [0, 100], color='red')
 plt.title("Predicted vs Actual Scores")
 plt.xlabel("Actual Scores")
 plt.ylabel("Predicted Scores")
